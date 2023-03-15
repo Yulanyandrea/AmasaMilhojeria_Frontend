ff32b661716f69bd0dd67f9ebd90877008d7afac
Yulany Munevar <andra_852@hotmail.com>
2023-03-14 19:51:16 -0500

    principal page form

--- src/pages/PrincipalPage/PrincipalPage.jsx
+++ src/pages/PrincipalPage/PrincipalPage.jsx
@@ -15,21 +15,31 @@ const PrincipalPage = () => {
 
         <h1 className="PrincipalPage__title">Bienvenido a Amasa Milhojeria</h1>
       </header>
-      <section className="PrincipalPage__section1">
-        <button type="submit" className="PrincipalPage__Section1--button" onClick={handleClick}>
-          <h2 className="PrincipalPage__section1--text">Historia</h2>
-          {/* <img src="/images/cake.jpg" alt="" className="PrincipalPage__Section1--img" /> */}
-        </button>
-      </section>
-      <section className="PrincipalPage__section2">
-        <h2 className="PrincipalPage__section2--text">Milhojas</h2>
-      </section>
-      <section className="PrincipalPage__section3">
-        <h2 className="PrincipalPage__section3--text">Pan</h2>
-      </section>
-      <section className="PrincipalPage__section4">
-        <h2 className="PrincipalPage__section4--text">Contactanos</h2>
-      </section>
+      <div className="PrincipalPage__section">
+        <section className="PrincipalPage__section1">
+          <button type="submit" className="PrincipalPage__Section1--button" onClick={handleClick}>
+            <h2 className="PrincipalPage__section1--text">Nosotros</h2>
+            {/* <img src="/images/cake.jpg" alt="" className="PrincipalPage__Section1--img" /> */}
+          </button>
+        </section>
+        <section className="PrincipalPage__section2">
+          <button type="submit" className="PrincipalPage__Section1--button" onClick={handleClick}>
+            <h2 className="PrincipalPage__section2--text">Milhojas</h2>
+          </button>
+        </section>
+        <section className="PrincipalPage__section3">
+          <button type="submit" className="PrincipalPage__Section1--button" onClick={handleClick}>
+            <h2 className="PrincipalPage__section3--text">Pan</h2>
+          </button>
+        </section>
+        <section className="PrincipalPage__section4">
+          <button type="submit" className="PrincipalPage__Section1--button" onClick={handleClick}>
+            <h2 className="PrincipalPage__section4--text">Contactanos</h2>
+          </button>
+        </section>
+
+      </div>
+
     </section>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,13 @@
+import { useNavigate } from 'react-router-dom';
 import Polly from '../../components/Polly/Polly';
 import './style.css';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const handleClick = (e) => {
+    e.preventDefault();
+    navigate('/Amasa_Home');
+  };
   return (
     <section className="ContainerHome">
       <div className="ContainerHome__image">
@@ -12,7 +18,7 @@ const Home = () => {
 
       </div>
       <div className="ContainerHome__button">
-        <button type="submit" className="ContainerHome__button--btn">Ingresar</button>
+        <button type="submit" className="ContainerHome__button--btn" onClick={handleClick}>Ingresar</button>
       </div>
 
       <div className="ContainerHome__Dogs">
