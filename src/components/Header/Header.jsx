c7c60b20c0252255018707ec7336fe44eb0e7b90
Yulany Munevar <andra_852@hotmail.com>
2023-03-31 13:29:26 -0500

    css cake's page

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,8 +5,10 @@ import './style.css';
 const Header = () => {
   return (
     <section className="containerHeader">
-      <img src="./images/cake.png" alt="" className="containerHeader__image" />
-      <h1 className="containerHeader__title">Amasa Milhojeria</h1>
+      <section className="containerHeader__amasaDetails">
+        <img src="./images/cake.png" alt="" className="containerHeader__image" />
+        <h1 className="containerHeader__title">Amasa Milhojeria</h1>
+      </section>
       <section className="containerHeader__shoppingCart">
         <button type="submit" className="containerHeader__shoppingCart--btn">
           <FontAwesomeIcon icon={faCartShopping} />
