15d852792c90129190a8738c31510860a941fc52
Yulany Munevar <andra_852@hotmail.com>
2023-03-31 13:09:09 -0500

    header component advanced

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,16 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+import './style.css';
+
 const Header = () => {
   return (
     <section className="containerHeader">
-      <img src="" alt="" className="containerHeader__image" />
+      <img src="./images/cake.png" alt="" className="containerHeader__image" />
+      <h1 className="containerHeader__title">Amasa Milhojeria</h1>
       <section className="containerHeader__shoppingCart">
-        <button type="submit" className="containerHeader__shoppingCart--btn">cart</button>
+        <button type="submit" className="containerHeader__shoppingCart--btn">
+          <FontAwesomeIcon icon={faCartShopping} />
+        </button>
       </section>
     </section>
 
