6b9344eaa0952e3766f36b59e45a66af11a7c72b
Yulany Munevar <andra_852@hotmail.com>
2023-04-28 16:53:01 -0500

    implementing redux Toolkit

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from '../feature/products/productSlice';
-import userReducer from '../feature/users/userSlice';
+// import userReducer from '../feature/users/userSlice';
 
 const store = configureStore({
   reducer: {
     products: productReducer,
-    users: userReducer,
   },
   devTools: process.env.NODE_ENV !== 'production',
 });
